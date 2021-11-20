592fa56c433be6ffdc32d3febf572d9b38023b81
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2021-11-20 15:51:01 +0530

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { LandingPage } from "./Components/LandingPage/LandingPage";
 function App() {
   return (
     <div className="App">
-
+        {/* hi */}
        <LandingPage/>
     </div>
   );
